7c0d70fad67f03da426a2c7a280e7a57d835f9ba
rjrege <rjrege@gmail.com>
2021-08-24 12:25:15 -0500

    8/24 intro and methods

--- Scripts/RR_Misc/EDA_new_gest_age.R
+++ Scripts/RR_Misc/EDA_new_gest_age.R
@@ -7,11 +7,11 @@ library(RColorBrewer)
 library(haven)
 
 # Input Data
-raw_pheno_gest_impute <- read_csv(file = here::here("Output/Data/raw_pheno_gest_impute.csv"))
+raw_pheno_gest <- read_csv(file = here::here("Output/Data/raw_pheno_w_ga.csv"))
 
 # Extract relevant vars
-imp_gest_vars <- raw_pheno_gest_impute %>%
-  select(uncchdid, monthprg, dayprg, yearprg, sample_group, sample_type, iccsex, date_prg_term, hour, minute, am_pm, sourcewt, date_icc_meas, icc_weight_birth, icc_length_home, icc_weight_home, icc_armcircm_home, icc_tricep_mean_home, icc_bicep_mean_home, icc_subscap_mean_home, icc_supra_mean_home, icc_calf_mean_home, icc_thigh_mean_home, icc_headcirc_mean_home, icc_abdom_mean_home, best_or_imputed_gestage_icc) %>%
+gest_vars <- raw_pheno_gest_impute %>%
+  select(uncchdid, monthprg, dayprg, yearprg, sample_group, sample_type, iccsex, date_prg_term, hour, minute, am_pm, sourcewt, date_icc_meas, icc_weight_birth, icc_length_home, icc_weight_home, icc_armcircm_home, icc_tricep_mean_home, icc_bicep_mean_home, icc_subscap_mean_home, icc_supra_mean_home, icc_calf_mean_home, icc_thigh_mean_home, icc_headcirc_mean_home, icc_abdom_mean_home, gestage) %>%
   filter(sample_type == "pregnancy") %>%
   na_if(-8) %>%
   mutate(icc_total_skin_mean_home = icc_tricep_mean_home + icc_bicep_mean_home + icc_subscap_mean_home + icc_supra_mean_home + icc_calf_mean_home + icc_thigh_mean_home) %>%
@@ -20,13 +20,13 @@ imp_gest_vars <- raw_pheno_gest_impute %>%
 ## Summary Statistics Overview
 
 
-skimr::skim(imp_gest_vars)
+skimr::skim(gest_vars)
 
 
 ## Distribution of Birth Weight
 
 
-imp_gest_vars %>%
+gest_vars %>%
   ggplot(mapping = aes(x = icc_weight_birth)) +
   geom_histogram()
 
@@ -34,7 +34,7 @@ imp_gest_vars %>%
 ## Distribution of Length Measured at Home
 
 
-imp_gest_vars %>%
+gest_vars %>%
   ggplot(mapping = aes(x = icc_length_home)) +
   geom_histogram()
 
@@ -42,7 +42,7 @@ imp_gest_vars %>%
 ## Distribution of Weight Measured at Home
 
 
-imp_gest_vars %>%
+gest_vars %>%
   ggplot(mapping = aes(x = icc_weight_home)) +
   geom_histogram()
 
@@ -50,7 +50,7 @@ imp_gest_vars %>%
 ## Distribution of Arm Circumference Measured at Home
 
 
-imp_gest_vars %>%
+gest_vars %>%
   ggplot(mapping = aes(x = icc_armcircm_home)) +
   geom_histogram()
 
@@ -58,7 +58,7 @@ imp_gest_vars %>%
 ## Distribution of Total Skin Fold Thickness Measured at Home
 
 
-imp_gest_vars %>%
+gest_vars %>%
   ggplot(mapping = aes(x = icc_total_skin_mean_home)) +
   geom_histogram()
 
@@ -66,7 +66,7 @@ imp_gest_vars %>%
 ## Distribution of Head Circumference Measured at Home
 
 
-imp_gest_vars %>%
+gest_vars %>%
   ggplot(mapping = aes(x = icc_headcirc_mean_home)) +
   geom_histogram()
 
@@ -74,23 +74,23 @@ imp_gest_vars %>%
 ## Distribution of Abdominal Circumference Measured at Home
 
 
-imp_gest_vars %>%
+gest_vars %>%
   ggplot(mapping = aes(x = icc_abdom_mean_home)) +
   geom_histogram()
 
 
-## Distribution of Gestational Age at Birth (New Imputed Code)
+## Distribution of Gestational Age at Birth
 
 
-imp_gest_vars %>%
-  ggplot(mapping = aes(x = best_or_imputed_gestage_icc)) +
+gest_vars %>%
+  ggplot(mapping = aes(x = gestage)) +
   geom_histogram()
 
 
 ## Distribution of Age at Measurement at Home (Date of Measurement - Date of End of Pregnancy) --> seems fishy that there are measurements that are nearly/over a month after birth. What to do here?
 
 
-imp_gest_vars %>%
+gest_vars %>%
   ggplot(mapping = aes(x = measurement_age)) +
   geom_histogram()
 
@@ -98,8 +98,8 @@ imp_gest_vars %>%
 ## Correlation Plot between various Phenotypic Variables
 
 
-imp_gest_vars2 <- imp_gest_vars %>% select(icc_weight_birth, icc_length_home, icc_weight_home, icc_armcircm_home, icc_headcirc_mean_home, icc_abdom_mean_home, icc_total_skin_mean_home)
-M <- cor(imp_gest_vars2, use = "complete.obs")
+gest_vars2 <- gest_vars %>% select(icc_weight_birth, icc_length_home, icc_weight_home, icc_armcircm_home, icc_headcirc_mean_home, icc_abdom_mean_home, icc_total_skin_mean_home)
+M <- cor(gest_vars2, use = "complete.obs")
 corrplot(M, type="upper", order="hclust",
          col=brewer.pal(n=8, name="RdYlBu"))
 
